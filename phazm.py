f886d572cf0b727b6d44e23843dcec2dc93e9ca8
s. rannou <mxs@sbrk.org>
2014-07-15 12:52:30 +0200

    Added db to avoid to have name conflicts.

--- phazm.py
+++ phazm.py
@@ -1,38 +1,47 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import argparse
 import random
 import sys
 import sqlite3
+import datetime
 
 MAX_LEN = 5
 MIN_LEN = 2
+DB_PATH = 'phazms.db'
 
-def exists(name):
-    return False
+class Phazms:
+    def __init__(self):
+        self.db = sqlite3.connect(DB_PATH)
+        self.cursor = self.db.cursor()
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS phazms(name TEXT, birthdate TEXT)')
 
-def register(name):
-    return True
+    def phazm(self):
+        """
+        Generates a unique name for phazms.
+        """
+        chunks = ['Jean', 'Jacques', 'Georges', 'Michel', 'Marcel', 'Raymond', 'Robert', 'Regis', 'Eugene', 'Francois', 'Yves', 'Yvette', 'Gertrude', 'Brigitte', 'Micheline', 'Oui', 'Non', 'Un-Gomme', 'Gros']
+        strength = MIN_LEN + int(random.random() * (MAX_LEN - MIN_LEN))
+        phazm = ''
+        for i in range(strength):
+            if i:
+                phazm += '-'
+            phazm += chunks[int(len(chunks) * random.random())]
+        id = 0
+        while self.exists(phazm):
+            id += 1
+        if id > 0:
+            phazm = phazm + ' ' + str(id)
+        self.register(phazm)
+        return phazm
 
-def phazm():
-    """
-    Generates a unique name for phazms.
-    """
-    chunks = ['Jean', 'Jacques', 'Georges', 'Michel', 'Marcel', 'Raymond', 'Robert', 'Régis', 'Eugène', 'François', 'Yves', 'Yvette', 'Gertrude', 'Brigitte', 'Micheline', 'Oui', 'Non', 'Un-Gomme', 'Gros']
-    strength = MIN_LEN + int(random.random() * (MAX_LEN - MIN_LEN))
-    phazm = ''
-    for i in range(strength):
-        if i:
-            phazm += '-'
-        phazm += chunks[int(len(chunks) * random.random())]
-    id = 0
-    while exists(phazm):
-        id += 1
-    if id > 0:
-        phazm = phazm + ' ' + str(id)
-    register(phazm)
-    return phazm
+    def exists(self, name):
+        return self.cursor.execute("SELECT COUNT(*) FROM phazms WHERE name=?", (name, )).fetchone() == 0
+
+    def register(self, name):
+        self.cursor.execute('INSERT INTO phazms(name, birthdate) VALUES(?, ?)', (name, str(datetime.datetime.now())))
+        self.db.commit()
 
 if __name__ == '__main__':
-    print 'Bienvenue à %s sur la planète Terre, on va bien s\'amuser!' % phazm()
+    p = Phazms()
+    print 'Bienvenue a %s sur la planete Terre, on va bien s\'amuser!' % p.phazm()
